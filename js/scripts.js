ec75233abcefaff66137495c692e6b49c8548b89
Matt Rosanio <mmrosani@syr.edu>
2016-01-29 08:34:29 -0800

    add a method to Pizza which returns a price

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,31 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.calculatePrice = function() {
+  var sizes = ['small','medium','large','xtra-large'];
+  var meats = ['pepperoni','bacon','sausage','chicken','beef','anchovies'];
+  var otherToppings = ['peppers','onions','mushrooms','jalapenos','pineapple','tomatos','olives'];
+  var price = 8;
+  for(var i = 0; i < sizes.length; i++) {
+    if(this.size === sizes[i]) {
+      price += (2*i);
+    }
+  }
+  for(var j = 0; j < this.toppings.length; j++) {
+    for(var k = 0; k < meats.length; k++) {
+      if(this.toppings[j] === meats[k]) {
+        price += 2;
+      }
+    }
+    for(var x = 0; x < otherToppings.length; x++) {
+      if(this.toppings[j] === otherToppings[x]) {
+        price += 1;
+      }
+    }
+  }
+  price += (price*0.08);
+  Math.round((100*price)/100);
+  console.log(price);
+  return price;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,10 @@
 describe('Pizza', function() {
   it('will create a new pizza object constructor with properties for size and toppings', function() {
     var testPizza = new Pizza('small',['pepperoni']);
-    expect(testPizza.size).to.equal('small');
-  })
+    expect(testPizza.toppings).to.eql(['pepperoni']);
+  });
+  it('will return a price based on size and toppings chosen', function() {
+    var testPizza = new Pizza('medium',['pineapple','bacon']);
+    expect(testPizza.calculatePrice()).to.equal(14.04);
+  });
 })
